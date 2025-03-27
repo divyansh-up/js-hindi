f8ff22b6eae79438f41b8b204961be97c1ede41f
divyansh-up <149304291+divyansh-up@users.noreply.github.com>
2025-03-27 10:59:12 +0000

    begning

---
+++ basics/1.js
@@ -0,0 +1,12 @@
+accountid=1234556
+let email="divyansh@gmail.com"
+var password="123333"
+let yes;
+city="delhi"
+ email="aggarwal@.com"
+ password=2222
+ city="chennai"
+  
+ console.table([accountid,email,password,city,yes]);
+ 
+
